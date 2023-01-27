995486578d6efb2192e26b4074e85b7cbfc022bc
kitarp29 <kitarpsinghrajpoot@gmail.com>
2023-01-28 01:33:24 +0530

    Client's IP can be accessed
    Signed-off-by: kitarp29 <kitarpsinghrajpoot@gmail.com>

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser');
+const http = require("http");
 
 
 app.use(bodyParser.urlencoded({ extended: false })); 
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
 
 app.post('/submit', (req, res) => {
     console.log(req.body);
+    console.log(req.headers['x-forwarded-for']);
   const rs = {
      username: req.body.email,
   password: req.body.password

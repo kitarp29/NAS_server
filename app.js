945110b3d8d7ee3356e8e1da5003a55241f59872
kitarp29 <kitarpsinghrajpoot@gmail.com>
2023-01-28 23:43:34 +0530

    Upload built
    Signed-off-by: kitarp29 <kitarpsinghrajpoot@gmail.com>

--- app.js
+++ app.js
@@ -3,26 +3,35 @@ const app = express()
 const path = require('path')
 const bodyParser = require('body-parser');
 const http = require("http");
+const multer = require('multer');
 
 
 app.use(bodyParser.urlencoded({ extended: false })); 
 
 const port = 3000
 
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
-    //res.send("done");
 })
 
-app.post('/submit', (req, res) => {
-
-
-     var username= req.body.email;
+app.post('/login', (req, res) => {
+  var username= req.body.email;
   var password = req.body.password;
   var ipAddress= req.headers['x-forwarded-for'];
 
-  if(ipAddress=="" && username=="test@gmail.com" && password=="1234"){
-  res.redirect("YHA LINK HOGA JISPE REDIRECT KARNA HAI");
+  if(username=="test@gmail.com" && password=="1234"){
+    res.sendFile(path.join(__dirname, '/page2.html'));
   }
   else
   {
@@ -30,6 +39,16 @@ app.post('/submit', (req, res) => {
   }
 
 })
+
+app.post('/read', (req, res) => {
+  res.redirect("YHA LINK HOGA JISPE REDIRECT KARNA HAI");
+})
+
+app.post('/write', upload.single('myFile'), (req, res) => {
+  // Do something with the file
+  res.send('File uploaded!');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
